606e02f0de1688f68af08acf3f66f12126c25566
JonJLopez <jonlop1121@gmail.com>
2024-12-17 00:41:05 -0500

    add word count function

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ def main():
     with open(path_to_file) as f:
         file_contents = f.read()
     
-    print(file_contents)
+    word_count = count_words(file_contents)
+    print(f"Word count is: {word_count}")
+
+#counts word in a file
+def count_words(file):
+    return len(file.split()) # return the length of the list of words
+
 
 main()
